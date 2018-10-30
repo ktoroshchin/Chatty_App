c3a389905bd4481b5cc56b178bddcfac6dfd693d
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-10-30 22:58:58 +0000

    Messages are sent and rendered, no websocket👌

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,14 +2,25 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+
   render() {
+    const  { sendMessage, currentUser, messages } = this.props;
+    const inputAreaMessage = event => {
+      const textArea = event.target;
+      if(event.charCode == 13){
+        const newMessage = textArea.value
+        sendMessage(newMessage)
+        textArea.value = "";
+      }
+    }
+
+
+
     return (
-
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" defaultValue={currentUser.name} placeholder="Your Name (Optional)" />
+        <input className="chatbar-message" type="text" onKeyPress={inputAreaMessage} placeholder="Type a message and hit ENTER" />
       </footer>
-
     );
   }
 }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,8 +3,18 @@ import Message from './Message.jsx';
 
 class MessageList extends Component {
   render() {
+    const { messages } = this.props;
+    console.log(messages);
+    let userMessage = messages.map((message) => {
+      return <Message userName={message.username} content={message.content} key={message.id}/>
+    })
     return (
-      <Message />
+      <main className="messages">
+        {userMessage}
+        <div className="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>
+      </main>
     )
   }
 }
