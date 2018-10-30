c7d91577dfcdd2c45caef6c25c9c4880654d46de
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-10-30 00:42:03 +0000

    Breaking App into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
+// import ChatBar from './ChatBar.jsx';
+// import Message from './Message.jsx';
+
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+
     );
   }
 }
